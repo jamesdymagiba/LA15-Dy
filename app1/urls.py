dbc24da8c780d1108f71271d03ed84a4cc03b61c
James Dy <150773927+jamesdymagiba@users.noreply.github.com>
2024-12-10 17:09:17 +0800

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('hello/', views.say_hello, name='say_hello'),
     path('hi/', views.say_hi, name='say_hi'),
-    path('posts/', views.blog_list, name='blog_list'),  # Name for blog list
-    path('posts/<int:id>/', views.blog_detail, name='blog_detail'),  # Name for blog detail
+    path('posts/', views.blog_list, name='blog_list'), 
+    path('posts/<int:id>/', views.blog_detail, name='blog_detail'),  
 ]
